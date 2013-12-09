abfdf0468f3c28aa7527e427bd2106760b6b551a
ramonsantos <ramonsantos.pe@gmail.com>
2013-12-09 02:02:11 -0300

    Adicionado um botão na Activity ProtocolsActivity

--- src/com/example/corporalevaluation/ProtocolsActivity.java
+++ src/com/example/corporalevaluation/ProtocolsActivity.java
@@ -3,6 +3,8 @@ package com.example.corporalevaluation;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class ProtocolsActivity extends Activity {
 
@@ -17,6 +19,10 @@ public class ProtocolsActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.protocols, menu);
 		return true;
+	}
+	
+	public void exibirToastClick(View v){
+		Toast.makeText(this, "Botão funcionando!", Toast.LENGTH_LONG).show();
 	}
 
 }
